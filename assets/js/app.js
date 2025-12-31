422db866ef43d16ae7a5d27fe68dbadf36359721
dominikf974-rgb <dominikf974@gmail.com>
2025-12-31 14:52:51 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,1 @@
-// app.js (Cookie/Ads entfernt – wird jetzt über footer.html geregelt)
-(function(){
-  // Falls später anderes JS in app.js kommt, bleibt diese Datei gültig.
-})();
+(function(){})();
